6aa28551e8ca66b66783e8f3208eee6c1eaf65f2
chrwoizi <christian@woizischke.de>
2014-05-07 00:29:33 +0200

    animation file importer and animation controller

---
+++ BaamStudios.AnimationController/Animation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using Microsoft.Xna.Framework;
+using SkinnedModel;
+
+namespace BaamStudios.AnimationController
+{
+    internal class Animation
+    {
+        internal readonly string Name;
+        internal readonly AnimationPlayer AnimationPlayer;
+        internal readonly SkinningData SkinningData;
+
+        internal Animation(string name, Stream file)
+        {
+            Name = name;
+            SkinningData = Load(file);
+            AnimationPlayer = new AnimationPlayer(SkinningData);
+        }
+
+        private static SkinningData Load(Stream file)
+        {
+            return (SkinningData) new BinaryFormatter().Deserialize(file);
+
+            // old json importer. changed to binary because json files are twice the size.
+
+            //JsConfig<Matrix>.DeSerializeFn = r =>
+            //{
+            //    if (string.IsNullOrEmpty(r) || r.Length < 3)
+            //        return Matrix.Identity;
+
+            //    var values = r.Substring(1, r.Length - 2);
+            //    var split = values.Split(',');
+            //    if (split.Length != 16)
+            //        return Matrix.Identity;
+
+            //    var result = new Matrix();
+            //    var i = 0;
+            //    result.M11 = float.Parse(split[i++], CultureInfo.InvariantCulture);
+            //    result.M12 = float.Parse(split[i++], CultureInfo.InvariantCulture);
+            //    result.M13 = float.Parse(split[i++], CultureInfo.InvariantCulture);
+            //    result.M14 = float.Parse(split[i++], CultureInfo.InvariantCulture);
+            //    result.M21 = float.Parse(split[i++], CultureInfo.InvariantCulture);
+            //    result.M22 = float.Parse(split[i++], CultureInfo.InvariantCulture);
+            //    result.M23 = float.Parse(split[i++], CultureInfo.InvariantCulture);
+            //    result.M24 = float.Parse(split[i++], CultureInfo.InvariantCulture);
+            //    result.M31 = float.Parse(split[i++], CultureInfo.InvariantCulture);
+            //    result.M32 = float.Parse(split[i++], CultureInfo.InvariantCulture);
+            //    result.M33 = float.Parse(split[i++], CultureInfo.InvariantCulture);
+            //    result.M34 = float.Parse(split[i++], CultureInfo.InvariantCulture);
+            //    result.M41 = float.Parse(split[i++], CultureInfo.InvariantCulture);
+            //    result.M42 = float.Parse(split[i++], CultureInfo.InvariantCulture);
+            //    result.M43 = float.Parse(split[i++], CultureInfo.InvariantCulture);
+            //    result.M44 = float.Parse(split[i++], CultureInfo.InvariantCulture);
+
+            //    return result;
+            //};
+            //var serializer = new JsonSerializer<SkinningData>();
+            //return serializer.DeserializeFromString(new StreamReader(file).ReadToEnd());
+        }
+
+        internal void Start()
+        {
+            AnimationPlayer.StartClip(SkinningData.AnimationClips.First().Value);
+            AnimationPlayer.Update(TimeSpan.Zero, true, Matrix.Identity);
+        }
+    }
+}
---
+++ BaamStudios.AnimationController/AnimationController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace BaamStudios.AnimationController
+{
+    /// <summary>
+    /// Manages multiple animations and provides access to the bone transforms.
+    /// </summary>
+    public class AnimationController
+    {
+        private readonly Dictionary<string, Animation> _animations = new Dictionary<string, Animation>();
+
+        private Animation _currentAnimation;
+
+        public string CurrentAnimationName
+        {
+            get
+            {
+                return _currentAnimation != null ? _currentAnimation.Name : null;
+            }
+        }
+
+        public void AddAnimation(string animationName, Stream animationFile)
+        {
+            _animations.Add(animationName, new Animation(animationName, animationFile));
+        }
+
+        public void PlayAnimation(string animationName, bool restartIfPlaying)
+        {
+            var animation = _animations[animationName];
+
+            if (!restartIfPlaying && _currentAnimation == animation)
+                return;
+
+            _currentAnimation = animation;
+            _currentAnimation.Start();
+        }
+
+        public void Update(GameTime gameTime, float speed)
+        {
+            if (_currentAnimation != null)
+                _currentAnimation.AnimationPlayer.Update(TimeSpan.FromSeconds(speed * (float)gameTime.ElapsedGameTime.TotalSeconds), true, Matrix.Identity);
+        }
+
+        /// <summary>
+        /// The relative transforms from bind pose to final bone position.
+        /// These values are used by the shader to transform the bindpose vertices to their final location.
+        /// </summary>
+        /// <returns></returns>
+        public Matrix[] GetSkinTransforms()
+        {
+            if (_currentAnimation == null)
+                return null;
+            return _currentAnimation.AnimationPlayer.GetSkinTransforms();
+        }
+
+        /// <summary>
+        /// The bone transforms (including bone position) relative to the model origin.
+        /// These values can be used to attach objects to bones, e.g. a gun to the hand.
+        /// See Microsoft.Xna.Framework.Graphics.Model.Bones for the bone names.
+        /// </summary>
+        /// <returns></returns>
+        public Matrix[] GetWorldTransforms()
+        {
+            if (_currentAnimation == null)
+                return null;
+            return _currentAnimation.AnimationPlayer.GetWorldTransforms();
+        }
+    }
+}
